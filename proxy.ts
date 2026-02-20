1e31861d3eccb76e78546caa294fb7b6d33ec601
robkebab <rpc219@gmail.com>
2026-02-19 19:22:28 -0600

    update proxy with cookie group value

--- proxy.ts
+++ proxy.ts
@@ -1,29 +1,35 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const ROLLING_RELEASE_COOKIE = "_vcrr";
+const ROLLING_RELEASE_PREFIX = "_vcrr";
 
 export function proxy(request: NextRequest) {
   const cookie = request.cookies.getAll();
 
-  const rrCookie = cookie.find((c) => c.name.startsWith(ROLLING_RELEASE_COOKIE));
+  const rrCookie = cookie.find((c) =>
+    c.name.startsWith(ROLLING_RELEASE_PREFIX),
+  );
 
   console.log(
     JSON.stringify({
       path: request.nextUrl.pathname,
       cookie: rrCookie?.value ?? null,
       timestamp: new Date().toISOString(),
-    })
+    }),
   );
 
   if (request.nextUrl.pathname.startsWith("/api/")) {
     const apiPath = request.nextUrl.pathname.replace(/^\/api\//, "");
 
     // Build a new URL object to manipulate query params easily
-    const proxiedUrlObj = new URL(`https://rr-back.vercel.zone/${apiPath}${request.nextUrl.search}`);
+    const proxiedUrlObj = new URL(
+      `https://rr-back.vercel.zone/${apiPath}${request.nextUrl.search}`,
+    );
+
+    const isReleaseCandidate = Number(rrCookie?.value?.split("|")[1]) < 1;
 
     // If rrCookie exists, add vcrrForceCanary=true to search params
-    if (rrCookie) {
+    if (isReleaseCandidate) {
       proxiedUrlObj.searchParams.set("vcrrForceCanary", "true");
     }
 
