d32be8e1ae33f4ea6919853228582b55d467baa3
robkebab <rpc219@gmail.com>
2026-02-19 23:31:11 -0600

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -1,24 +1,30 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { get } from "@vercel/edge-config";
 
 const ROLLING_RELEASE_PREFIX = "_vcrr";
 
-export function proxy(request: NextRequest) {
-  const cookie = request.cookies.getAll();
-
-  const rrCookie = cookie.find((c) =>
-    c.name.startsWith(ROLLING_RELEASE_PREFIX),
-  );
-
-  console.log(
-    JSON.stringify({
-      path: request.nextUrl.pathname,
-      cookie: rrCookie?.value ?? null,
-      timestamp: new Date().toISOString(),
-    }),
-  );
-
+export async function proxy(request: NextRequest) {
   if (request.nextUrl.pathname.startsWith("/api/")) {
+    const cookie = request.cookies.getAll();
+
+    const rrCookie = cookie.find((c) =>
+      c.name.startsWith(ROLLING_RELEASE_PREFIX),
+    );
+
+    console.log(
+      JSON.stringify({
+        path: request.nextUrl.pathname,
+        cookie: rrCookie?.value ?? null,
+        timestamp: new Date().toISOString(),
+      }),
+    );
+
+    const [deployment = "unknown", percentage = -1] = await Promise.all([
+      get<string>("targetDeploymentId"),
+      get<number>("targetPercentage"),
+    ]);
+
     const apiPath = request.nextUrl.pathname.replace(/^\/api\//, "");
 
     // Build a new URL object to manipulate query params easily
@@ -26,7 +32,10 @@ export function proxy(request: NextRequest) {
       `https://rr-back.vercel.zone/${apiPath}${request.nextUrl.search}`,
     );
 
-    const isReleaseCandidate = Number(rrCookie?.value?.split("|")[1]) < 1;
+    const [deploymentId = "", bucket = 0] = rrCookie?.value?.split("|") ?? [];
+
+    const isReleaseCandidate =
+      deployment === deploymentId && Number(bucket) < percentage;
 
     // If rrCookie exists, add vcrrForceCanary=true to search params
     if (isReleaseCandidate) {
